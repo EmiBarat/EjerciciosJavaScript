fa909ed55312cebdfd5305f99073dbfb53485973
Emibarat <emibarat@gmail.com>
2020-02-20 20:46:18 -0300

    Agrego primer ejercicio de JS completo

---
+++ Ejercicio001/js/AlertHolaMundo.js
@@ -0,0 +1,5 @@
+//Este es primer mensaje
+alert("Hola Mundo!");
+//este es el segundo mensaje
+alert("Soy el primer script");
+//podria haber hecho otro script para el segundo msj, quizas hubieraquedado mas prolijo
