e2b61254bae22f62ef2e15306d4a6f631100342d
Emibarat <emibarat@gmail.com>
2020-02-27 19:46:55 -0300

    Agrego ej de Promocionales, aprobados y reprovados

---
+++ EjManuel/Promocionales.js
@@ -0,0 +1,58 @@
+const rendidas = [
+    {
+      alumno:'Burgos',
+      examenA:8,
+      examenB:7,
+    },
+    {
+       alumno:'Parizia',
+      examenA:9,
+      examenB:7,
+    },
+    {
+       alumno:'Barat',
+      examenA:9,
+      examenB:6,
+    },
+    {
+       alumno:'Chavarria',
+      examenA:8,
+      examenB:8,
+    },  
+    {
+       alumno:'Lopez',
+      examenA:4,
+      examenB:3,
+    }  
+    ];
+
+    //definicion de los arreglos donde se guardan los apellidos de los alumnos
+    //dependiendo de su condicion
+    var alumnosPromocionados = [];
+    var alumnosNoPromocionados = [];
+    var alumnosDesaprobados = [];
+    
+    //Funcion que calcula el promedio en base a dos notas
+    function promedioNotas(a,b){
+        return (a + b)/2;
+    }
+
+    //funcion que determina la condicion del alumno y agrega su apellido en el arreglo correspondiente
+    function determinarCondicionAlumno(array){
+        for (let i = 0; i < array.length; i++) {
+            var promedio = promedioNotas(rendidas[i].examenA,rendidas[i].examenB);
+            if (promedio >= 8){
+                alumnosPromocionados[i] = rendidas[i].alumno;
+            }
+            else if(promedio >= 6){
+                alumnosNoPromocionados[i] = rendidas[i].alumno;
+            }
+            else {
+                alumnosDesaprobados[i] = rendidas[i].alumno;
+            }
+        }
+    }
+
+    console.log(alumnosPromocionados);
+    console.log(alumnosNoPromocionados);
+    console.log(alumnosDesaprobados);
